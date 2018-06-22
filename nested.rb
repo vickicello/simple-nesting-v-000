7a67e8f0a4b936270595e02a982c5830e9eaaa09
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 18:37:35 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -59,6 +59,7 @@ def dennis_ritchies_language
 end
 
 def adding_matz
+
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
@@ -81,7 +82,8 @@ def adding_matz
         }
      }
 
-
+		programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+	 	programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "C"
 end
 
 def changing_alan
